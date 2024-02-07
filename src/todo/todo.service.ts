f8e432a15d5c268909f62c3070e94690a03934ca
VoN <sudo.von.contact@gmail.com>
2024-02-06 20:21:28 -0600

    fix: :tada: initial commit

---
+++ src/todo/todo.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { Todo } from './entities/todo.entity';
+
+@Injectable()
+export class TodoService {
+  private todos: Todo[] = [{
+    id: 1,
+    description: 'todo-description',
+    done: false,
+  }];
+
+  create(createTodoDto: CreateTodoDto) {
+    return 'This action adds a new todo';
+  }
+
+  findAll(): Todo[] {
+    return this.todos;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} todo`;
+  }
+
+  update(id: number, updateTodoDto: UpdateTodoDto) {
+    return `This action updates a #${id} todo`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} todo`;
+  }
+}
